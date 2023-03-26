8c99c159420fa61ee5f0d7701a6bf2fd66574645
Fayzullo07 <jorayevfayzillo77@gmail.com>
2023-03-27 01:49:58 +0500

    3d

---
+++ src/components/Tech.jsx
@@ -0,0 +1,17 @@
+import { SectionWrapper } from "../hoc";
+import { BallCanvas } from "./canvas";
+import { technologies } from "../constants";
+
+const Tech = () => {
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technologie) => (
+        <div className="w-28 h-28" key={technologie.name}>
+          <BallCanvas icon={technologie.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SectionWrapper(Tech, "");
